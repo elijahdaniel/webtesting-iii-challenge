8ca975043ae824ee8aa46d5de76077cbd3559e44
Elijah Paminsan <e.paminsan@gmail.com>
2019-10-08 09:22:59 -0700

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
-// Test away!
+import React from 'react'
+import { render } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+import Controls from './Controls.js'
+
+describe('<Controls />', () => {
+  it('button is disabled', () => {
+    const { getByText, getByTestId, container } = render(
+      <Controls locked={true} closed={true} />
+    )
+
+    const disabledButton = getByText(/b/i)
+
+    expect(getByTestId(container, disabledButton)).toBeDisabled()
+
+    expect(disabledButton).toHaveAttribute('disabled')
+  })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,42 @@
-// Test away
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import Dashboard from './Dashboard'
+import Controls from '../controls/Controls'
+import Display from '../display/Display'
+
+// import 'jest-dom/extend-expect'
+
+test('dashboard renders without crashing', () => {
+  render(<Dashboard />)
+})
+
+// creating snapshot to watch changes
+test('should match snapshot', () => {
+  expect(render(<Dashboard />)).toMatchSnapshot()
+})
+
+test('dashboard shows display and controls', () => {
+  render(
+    <Dashboard>
+      <Display />
+      <Controls />
+    </Dashboard>
+  )
+})
+
+test('displays if gate is open/closed and if it is locked/unlocked', () => {
+  const { getByText } = render(<Dashboard />)
+  getByText(/open/i)
+  getByText(/close/i)
+  getByText(/lock/i)
+  getByText(/unlocked/i)
+})
+
+test(`control's props should be calling functions`, () => {
+  // mock functions
+  const toggleLocked = jest.fn()
+  const toggleClosed = jest.fn()
+
+  render(<Controls toggleLocked={toggleLocked} toggleClosed={toggleClosed} />)
+})
